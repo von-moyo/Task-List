2732ddb0fc8659be596602617dd654da6424e05f
von-moyo <florishadekogbe@gmail.com>
2022-06-15 22:43:38 +0100

    final

--- app.js
+++ app.js
@@ -1,59 +1,37 @@
-// create a "close" button and append it to each list item
-
-var myList = document.getElementsByTagName('li');
-var i;
-for (let i = 0; i < myList.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myList[i].appendChild(span);
-}
-
-//clck on the close button to hide the current list item
-
-var close = document.getElementsByClassName('close');
-var i;
-for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
 //add a "checked" symbol when clicking on a list item
 
 var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
+list.addEventListener('click', function (evt) {
+    if (evt.target.tagName === 'LI') {
+        evt.target.classList.toggle('checked');
     }
 }, false);
 
-// create a new list wehn clicking on the add button
+// create a new list-item when clicking on the add button
 
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    var li = document.createElement("LI");
+    var inputValue = document.getElementById("myInput").valu
+    const task = document.createElement("input");
+    task.className = "textbox"
+    task.setAttribute('value', inputValue);
+    li.appendChild(task);
     if (inputValue === '') {
         alert("You must write something!");
     } else {
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
-
-    var span = document.createElement("SPAN");
+    var span = document.createElement("span");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.ParentElement;
-            div.style.display = "none";
-        }
-    }
+    span.addEventListener('click', () => { li.style.display = "none"; })
 }
+
+document.querySelector('#myInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        newElement();
+    }
+})
